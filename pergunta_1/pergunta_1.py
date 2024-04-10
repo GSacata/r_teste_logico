a079be290340f7ce8e8fb157393e012e767678dc
GSacata <gsacata@gmail.com>
2024-04-10 20:30:20 -0300

    commiting to try another way

--- pergunta_1/pergunta_1.py
+++ pergunta_1/pergunta_1.py
@@ -1,8 +1,10 @@
 # Pergunta 1
 
-array_exerc = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+# array_exerc = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+array_exerc = [2, 1, 5, 1, 1, 1, 7, 9, 13, 127, 21] #DEV-ER
 search_num = 1
 
+"""
 for pos, value in enumerate(array_exerc):
     print(f'{pos}, {value}')
 
@@ -13,11 +15,24 @@ for pos, value in enumerate(array_exerc):
         print(f'{pos}, {value}')
     else:
         pass
+"""
 
-# def align_values(num):
-#     pass
+def replace_values(num):
+    x = array_exerc.pop(num)
+    array_exerc.insert(0, x)
+
+def arrange_number(num):
+    for v in array_exerc:
+        if v == num:
+            replace_values(v)
+        else:
+            continue
 
 
+
+arrange_number(search_num)
+
+print(array_exerc)
 
 
 # array_exerc.sort()
