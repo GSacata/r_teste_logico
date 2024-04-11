c8334b782aee958e3d14ca42ac6aa900fdbd4584
GSacata <gsacata@gmail.com>
2024-04-11 20:02:53 -0300

    base for files created

---
+++ pergunta_3/pergunta_3.py
@@ -0,0 +1,17 @@
+# Pergunta 3
+
+array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
+num_input = 2
+possible_combos = []
+
+def check_combos():
+    pass
+
+combo_exists = check_combos(num_input)
+
+if combo_exists:
+    print(combo_exists)
+    print(f"Combinações existem\n{possible_combos}")
+else:
+    print(combo_exists)
+    print(f"Não existem combinações para o número inserido")
