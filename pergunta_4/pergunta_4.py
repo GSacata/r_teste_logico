650d46f8b3105e886e786a133e0d09df92cc6ace
GSacata <gsacata@gmail.com>
2024-04-12 19:48:48 -0300

    anti-repeated numbers

--- pergunta_4/pergunta_4.py
+++ pergunta_4/pergunta_4.py
@@ -1,4 +1,29 @@
 # Pergunta 4
 
+#TEST: Array proposto.
 array_exerc = [9, 2, 3, 1, 4]
 missing_numbers = []
+
+# #TEST: Array com número repetido (deve não checar números repetidos).
+# array_exerc = [9, 2, 3, 1, 4, 2, 4, 9]
+# missing_numbers = []
+
+
+
+existent_numbers = []
+
+def get_existent_num(param_array):
+    for item in param_array:
+        # existent_numbers.append(item) if item not in existent_numbers else ...
+        if item not in existent_numbers:
+            print(f"Appending {item}")
+            existent_numbers.append(item)
+        else:
+            print(f"{item} already checked")
+            pass
+
+get_existent_num(array_exerc)
+
+print("array_exerc", f"{array_exerc}")
+print("missing_numbers", f"{missing_numbers}")
+print("existent_numbers", f"{existent_numbers}")
