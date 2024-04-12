1f022480de15fcfd34ea978d192856e43c0c280e
GSacata <gsacata@gmail.com>
2024-04-11 21:53:45 -0300

    solved duplicate checks

--- pergunta_3/pergunta_3.py
+++ pergunta_3/pergunta_3.py
@@ -1,34 +1,38 @@
 # Pergunta 3
 
-# TEST: Valor proposto pelo exercício
-array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
-num_input = 2
-existent_combos = []
+# # TEST: Valor proposto pelo exercício
+# array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
+# num_input = 2
+# existent_combos = []
+# checked_combinations = []
 
 
 # # TEST: Valor possível de sua escolha
 # array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
 # num_input = 7
 # existent_combos = []
+# checked_combinations = []
 
 
 # # TEST: Valor acima do máximo possível
 # array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
 # num_input = 100
 # existent_combos = []
+# checked_combinations = []
 
 
 # # TEST: Valor abaixo do mínimo possível
 # array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
 # num_input = -4
 # existent_combos = []
-
-
-# # TEST: Testar in_array_count com 8 (seria um 4 + 4, mas não existem dois 4 no array)
-# array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
-# num_input = 8
-# existent_combos = []
 # checked_combinations = []
+
+
+# TEST: Testar in_array_count com 8 (seria um 4 + 4, mas não existem dois 4 no array)
+array_exerc = [1, 15, 2, 7, 2, 5, 7, 1, 4]
+num_input = 8
+existent_combos = []
+checked_combinations = []
 
 
 # # TEST: Testar in_array_count com 8 (seria um 4 + 4, mas não existem dois 4 no array)
@@ -64,24 +68,33 @@ def check_combos(param_num):
         while not i > param_num:
             
             current_tuple = (i, j)
+            reverse_current_tuple = (j, i)
             in_array_count = array_exerc.count(i)
-            
 
-            if i in array_exerc and j in array_exerc:
-                if i != j:
-                    existent_combos.append(current_tuple)
-                    i += 1; j -= 1
-
-                elif i == j and in_array_count >= 2:
-                    existent_combos.append(current_tuple)
-                    i += 1; j -= 1
-                    break
-                
-                else:
-                    i += 1; j -= 1
-
-            else:
+            if reverse_current_tuple in checked_combinations:
+                print(f"{current_tuple} already checked")
                 i += 1; j -= 1
+            
+            else:
+                if i in array_exerc and j in array_exerc:
+                    if i != j:
+                        existent_combos.append(current_tuple)
+                        checked_combinations.append(current_tuple)
+                        i += 1; j -= 1
+
+                    elif i == j and in_array_count >= 2:
+                        existent_combos.append(current_tuple)
+                        checked_combinations.append(current_tuple)
+                        i += 1; j -= 1
+                        break
+                    
+                    else:
+                        checked_combinations.append(current_tuple)
+                        i += 1; j -= 1
+
+                else:
+                    checked_combinations.append(current_tuple)
+                    i += 1; j -= 1
     else:
         print(f"Função não executada")
     pass
