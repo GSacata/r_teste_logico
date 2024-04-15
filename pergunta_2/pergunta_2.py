ffcc97b370542fd087a24c54fcc0d2177493e6d1
GSacata <gsacata@gmail.com>
2024-04-15 19:40:52 -0300

    created basic files

---
+++ pergunta_2/pergunta_2.py
@@ -0,0 +1,9 @@
+# Pergunta 2
+
+"""
+valor node: valor
+l_br: None (vai receber outro node)
+r_br: None (vai receber outro node)
+l_flag: true ou false (indica se aquele caminho é válido ou não)
+r_flag: true ou false (indica se aquele caminho é válido ou não) 
+"""
